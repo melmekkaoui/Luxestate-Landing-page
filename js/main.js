ff00d832de8b9a62ae6ab2dac3692389198e7cac
Your Name <you@example.com>
2022-04-10 03:09:28 +0000

    Upload The Design

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**=======================Header Section ======================== */
+let iconbar = document.querySelector("i.icon-bars");
+let headerMenu = document.querySelector(".navbar-menu");
+iconbar.addEventListener("click",()=>{
+    headerMenu.classList.toggle("active");
+    iconbar.classList.toggle('activeicon')
+    
+});
+
+
+
+
+
+
+/**=======================Header Section ======================== */
